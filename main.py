a0fdd27be74dada088ae42dc44234a86c018d51a
waregin <warebec@gmail.com>
2024-07-29 08:52:49 -0400

    fixing bug where not every record has classifications

--- main.py
+++ main.py
@@ -7,19 +7,33 @@ input_val = input()
 output_vals = ["title;author;isbn;DDC"]
 while input_val != "DONE":
     # input
-    google_response = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:%s" % input_val).json()
-    open_library_response = requests.get("http://openlibrary.org/api/volumes/brief/isbn/%s.json" % input_val).json()
+    google_response = requests.get(
+        "https://www.googleapis.com/books/v1/volumes?q=isbn:%s" % input_val
+    ).json()
+    open_library_response = requests.get(
+        "https://openlibrary.org/api/volumes/brief/isbn/%s.json" % input_val
+    ).json()
 
     # output
-    volume_info = google_response.get("items")[0].get("volumeInfo")
+    google_items = google_response.get("items")
+    if google_items is not None:
+        volume_info = google_items[0].get("volumeInfo")
+    else:
+        volume_info = {"title": "", "authors": [""]}
     ddc = ""
-    records = open_library_response.get("records")
-    for key in records:
-        ddc = records[key].get("data").get("classifications").get("dewey_decimal_class")
-        if isinstance(ddc, collections.abc.Sequence):
-            ddc = ddc[0]
-        break
-    output_vals.append("%s;%s;%s;%s" % (volume_info.get("title"), volume_info.get("authors")[0], input_val, ddc))
+    if open_library_response is not None and type(open_library_response) is not list:
+        records = open_library_response.get("records")
+        for key in records:
+            classifications = records[key].get("data").get("classifications")
+            if classifications is not None:
+                ddc = classifications.get("dewey_decimal_class")
+                if isinstance(ddc, collections.abc.Sequence):
+                    ddc = ddc[0]
+                break
+    output_vals.append(
+        "%s;%s;%s;%s"
+        % (volume_info.get("title"), volume_info.get("authors")[0], input_val, ddc)
+    )
     input_val = input()
 
 for val in output_vals:
